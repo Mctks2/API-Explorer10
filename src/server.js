ba4d179aab17edf002a4420343e8e416d49fcc4f
Thiago <mctks2@hotmail.com>
2024-05-11 16:41:31 -0300

    improving image routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("express-async-errors");
 
 const migrationRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
+
 const express = require("express");
 const routes = require("./routes")
 
@@ -9,6 +11,8 @@ migrationRun();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 app.use(( error, request, response, next ) => {
